c7cde3ee0db1a92d976f8b7990393341911ba3ce
t33y <omotayoolarewaju@gmail.com>
2023-02-27 14:00:39 +0100

    Updates

---
+++ precache-manifest.1f19e42b381d97bb53e2d78228fd06ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b26a8bcd6026b1c1794",
+    "url": "/notes/static/css/main.cfac9647.chunk.css"
+  },
+  {
+    "revision": "8b26a8bcd6026b1c1794",
+    "url": "/notes/static/js/main.8b26a8bc.chunk.js"
+  },
+  {
+    "revision": "69fd6a60922ec64021c6",
+    "url": "/notes/static/js/1.69fd6a60.chunk.js"
+  },
+  {
+    "revision": "1291e1b612bc298497c9",
+    "url": "/notes/static/js/runtime~main.1291e1b6.js"
+  },
+  {
+    "revision": "9421847a4ec62a12caa2f1ce3759d609",
+    "url": "/notes/index.html"
+  }
+];
